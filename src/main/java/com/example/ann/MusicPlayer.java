7b8d337212ae5e1bc2ebf287f0a37230d7c73595
Anna Ryaskina <anna.ryaskina@abbyy.com>
2019-08-02 15:12:01 +0300

    Добавлена реализация DI с помощью сеттеров

--- src/main/java/com/example/ann/MusicPlayer.java
+++ src/main/java/com/example/ann/MusicPlayer.java
@@ -1,14 +1,45 @@
 package com.example.ann;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+
+    private String name;
+    private int volume;
     //IoC
-    public MusicPlayer(Music music){
-        this.music = music;
+    public MusicPlayer(List<Music> musicList){
+        this.musicList.addAll(musicList);
+    }
+
+    public MusicPlayer(){
+
+    }
+    public void setMusicList(List<Music> musicList) {
+        this.musicList.addAll(musicList);
     }
 
     public void playMusic(){
-        System.out.println("playing " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("playing " + music.getSong());
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
---
+++ src/main/java/com/example/ann/PopMusic.java
@@ -0,0 +1,8 @@
+package com.example.ann;
+
+public class PopMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Billie Jinn";
+    }
+}
