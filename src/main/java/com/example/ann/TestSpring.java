23fd7340b945046465d5045af066b6a3dbe9f6e0
Anna Ryaskina <anna.ryaskina@abbyy.com>
2019-08-05 13:16:31 +0300

    Добавлены init, destroy и factory методы

--- src/main/java/com/example/ann/TestSpring.java
+++ src/main/java/com/example/ann/TestSpring.java
@@ -5,11 +5,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        ClassicalMusic classicalMusic = context.getBean("musicBean2", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = firstMusicPlayer==secondMusicPlayer;
+
+        System.out.println(comparison);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+        //System.out.println(musicPlayer.getName());
+        //System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
